bdad554201147d620e26245fc9ecabafdc9c792b
KyleDiepCode <kylediep24@gmail.com>
2026-06-18 20:32:03 -0700

    Didnt save everything

--- app/Components/Manual.jsx
+++ app/Components/Manual.jsx
@@ -172,7 +172,7 @@ export default function ManualInputPage(){
                 })
                 }
                 <button>
-                    hello
+                    
                 </button>
 
             </div>
--- app/MainWebsite.jsx
+++ app/MainWebsite.jsx
@@ -5,15 +5,34 @@ import Splitter from "./Components/PersonItemSplit";
 import TitleAndButtons from "./Components/FrontPage"
 import ManualInputPage from "./Components/Manual"
 
+
+
 /*
- to do list:
 
-1. Get a color set for each person // can do later
-2. set up a flex-wrap inside item box so when selected the color shows
-3. Add color to each person
-4. Have a way for the person to choose a color
-5. when click on the item add the color
+Finished:
+* Get a color set for each person // can do later. ✔
+* set up a flex-wrap inside item box so when selected the color shows✔
+* Add color to each person ✔
+* when click on the item add the color ✔
+* Getting to when you create an item it sets it into the text box✔
 
+
+
+Big Task list:
+* Calculate Button will send to a seperate page
+* Each Person with the item under them and the breakdown tip etc
+* Style it so mobile doesnt look atrotious
+* do the parsing part for the reciept
+
+Current Task:  Calculate Button will send to a seperate page
+    sub task:
+    *
+
+
+Maybe:
+* Have a way for the person to choose a color // maybe come back to this 
+* Keep in mind mobile will raise up the keyboard so might take out focus for mobile
+* Mobile styling is kind of wrack
 */
 
 
